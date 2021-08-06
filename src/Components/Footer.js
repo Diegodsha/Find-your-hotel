d40daf4f4e55698a8fce36dc74060bb918f3239b
Diego <diegodsha@hotmail.com>
2021-08-06 18:16:32 -0500

    Initial Footer component setup

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => (
+  <footer className="d-flex flex-column align-items-center">
+    <span>SQUARE</span>
+    <span>
+      Punta Nizuc - Cancún 335, Zona Hotelera, 77500 Cancún, Q.R. +998-825-32-28
+    </span>
+    <span>
+      <span>facebook</span>
+      <span>twitter</span>
+      <span>instagram</span>
+    </span>
+    <span>Square 2021. All rights reserved</span>
+  </footer>
+);
+
+export default Footer;
