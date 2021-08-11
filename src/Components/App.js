342caffb01d44437ac93001814744291c494d220
Diego <diegodsha@hotmail.com>
2021-08-11 14:36:36 -0500

    Add route to details & favourites

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,15 @@
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import { BASE_URL, hotelsEndpoint } from '../API/EndPoints';
-import { setLogginAction, setLogoutAction } from '../Actions';
+import { fetchHotels, setLogginAction, setLogoutAction } from '../Actions';
 import Hotels from './Hotels';
 import Home from './Home';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
-import Nav from './Nav';
+import HotelDetails from './HotelDetails';
+import Favourites from './Favourites';
 
 function App() {
   const user = useSelector((state) => state.user);
@@ -21,15 +20,18 @@ function App() {
         withCredentials: true,
       })
       .then((res) => {
-        console.log(res.data);
         if (res.data.logged_in && user.loggedIn === false) {
           dispatch(setLogginAction(res.data.user));
         } else if (!res.data.logged_in && user.loggedIn === true) {
+          localStorage.removeItem('user');
           dispatch(setLogoutAction());
         }
-      })
-      .catch((err) => console.log('loggin status error:', err));
+      });
   };
+
+  useEffect(() => {
+    dispatch(fetchHotels());
+  }, []);
 
   useEffect(() => {
     checkLogginStatus();
@@ -42,8 +44,8 @@ function App() {
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/hotels" component={Hotels} />
-        {/* <Route exact path="/hotels/:id" component={Home} />
-        <Route exact path="/favourites" component={Home} /> */}
+        <Route exact path="/hotels/details/:id" component={HotelDetails} />
+        <Route exact path="/favourites" component={Favourites} />
         <Home />
       </Switch>
     </Router>
