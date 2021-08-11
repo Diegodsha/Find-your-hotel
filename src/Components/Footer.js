dbbdd267b433064984f5cd1dd2e6605ca6b29f6c
Diego <diegodsha@hotmail.com>
2021-08-11 14:36:58 -0500

    Add links to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,14 +8,20 @@ import { BrandName } from '../Assets/StyledHome';
 
 const Footer = () => (
   <StyledFooter className="d-flex flex-column align-items-center">
-    <BrandName>SQUARE</BrandName>
+    <a className="text-dark text-decoration-none" href="#home"><BrandName>SQUARE</BrandName></a>
     <span className="text-secondary my-4">
       Punta Nizuc - Cancún 335, Zona Hotelera, 77500 Cancún, Q.R. +998-825-32-28
     </span>
     <span className="mb-4">
-      <TiSocialFacebook className="social facebook" />
-      <TiSocialTwitter className="social twitter" />
-      <TiSocialInstagram className="social instagram" />
+      <a href="#face">
+        <TiSocialFacebook className="social facebook" />
+      </a>
+      <a href="#twit">
+        <TiSocialTwitter className="social twitter" />
+      </a>
+      <a href="#insta">
+        <TiSocialInstagram className="social instagram" />
+      </a>
     </span>
     <span className="text-secondary">Square 2021. All rights reserved</span>
   </StyledFooter>
