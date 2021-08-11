76279ec08e232502acf52e165c2fd0b67a399389
Diego <diegodsha@hotmail.com>
2021-08-11 14:43:20 -0500

    Update hotels component

--- src/Components/Hotels.js
+++ src/Components/Hotels.js
@@ -1,15 +1,9 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchHotels } from '../Actions';
+import { useSelector } from 'react-redux';
 import HotelList from '../Containers/HotelList';
 
 const Hotels = () => {
-  const dispatch = useDispatch();
   const hotels = useSelector((state) => state.hotels);
 
-  useEffect(() => {
-    dispatch(fetchHotels());
-  }, []);
   return (
     <div>
       <HotelList hotels={hotels} />
