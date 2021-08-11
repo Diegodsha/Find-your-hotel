679c4127a623a001b3dba0cdfd85f2882f0ba5b6
Diego <diegodsha@hotmail.com>
2021-08-11 14:45:19 -0500

    Style review card

---
+++ src/Assets/StyledReview.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledReview = styled.div`
+  border-radius: 15px;
+  padding: 30px;
+  box-shadow: 0px 0px 15px -2px #868686;
+`;
+
+export default StyledReview;
