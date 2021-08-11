79e51e40d6a36c4c03687a71413446fc9a6c568c
Diego <diegodsha@hotmail.com>
2021-08-11 14:43:01 -0500

    Update nav component

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,44 +1,103 @@
-const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-ligth fixed-top px-5">
-    <div className="container-fluid">
-      <a className="navbar-brand " href="#root">
-        Square
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div
-        className="collapse navbar-collapse justify-content-end"
-        id="navbarNav"
-      >
-        <ul className="navbar-nav justify-content-between">
-          <li className="nav-item d-lg-flex my-2 my-lg-0">
-            <a
-              className="nav-link active mx-3 books p-0 fw-bold"
-              aria-current="page"
-              href="#root"
-            >
-              Sign in
-            </a>
-          </li>
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import { BrandName } from '../Assets/StyledHome';
+import { BASE_URL, hotelsEndpoint } from '../API/EndPoints';
+import { setLogoutAction } from '../Actions';
+import StyledNav from '../Assets/StyledNav';
 
-          <li>
-            <a className="nav-link categories ms-3 p-0 text-white" href="#root">
-              Sign Up
-            </a>
-          </li>
-        </ul>
+const Nav = () => {
+  const user = useSelector((state) => state.user);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const handleLogOut = () => {
+    axios
+      .delete(`${BASE_URL}${hotelsEndpoint.logOutUser}`, {
+        withCredentials: true,
+      })
+      .then(() => {
+        localStorage.removeItem('user');
+        dispatch(setLogoutAction());
+        history.push('/');
+      });
+  };
+  return (
+    <StyledNav className="navbar navbar-expand-lg navbar-light bg-light fixed-top px-2 px-md-5 py-2">
+      <div className="container-fluid">
+        {user.loggedIn === false ? (
+          <Link to="/" className="text-dark text-decoration-none">
+            <BrandName>Square</BrandName>
+          </Link>
+        ) : (
+          <Link to="/" className="text-dark text-decoration-none">
+            <BrandName>{`${user.user.name ?? 'Square'}`}</BrandName>
+          </Link>
+        )}
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          {user.loggedIn === false ? (
+            <ul className="navbar-nav justify-content-between align-items-center">
+              <li className="nav-item d-lg-flex my-2 my-lg-0">
+                <Link
+                  to="/signin"
+                  className="nav-link active mx-3 p-0 fw-bold"
+                  aria-current="page"
+                >
+                  Sign in
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/signup" className="nav-link categories ms-3 logout">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className="navbar-nav justify-content-between">
+              <li className="nav-item d-lg-flex my-3 my-lg-0">
+                <Link
+                  to="/"
+                  className="nav-link active mx-3 p-0 fw-bold"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item d-lg-flex mb-3 my-lg-0">
+                <Link
+                  to="/favourites"
+                  className="nav-link active mx-3 p-0 fw-bold"
+                >
+                  Favourites
+                </Link>
+              </li>
+
+              <li>
+                <button className="nav-link active mx-3 mb-3 mb-lg-0 fw-bold logout" type="button" onClick={() => handleLogOut()}>
+                  logout
+                </button>
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </StyledNav>
+  );
+};
 
 export default Nav;
