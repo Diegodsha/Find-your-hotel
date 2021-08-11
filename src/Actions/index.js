abbe5cb403b85e7b3bbbefd8824f62908d297384
Diego <diegodsha@hotmail.com>
2021-08-11 14:47:33 -0500

    Remove comments from styles

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import types from './Types';
 import getHotels from '../API/api';
 
@@ -28,6 +27,5 @@ export const setLogoutAction = () => ({
 
 export const fetchHotels = () => async (dispatch) => {
   const hotels = await getHotels();
-  //   console.log('waa', hotels);
   dispatch(fetchHotelsAction(hotels));
 };
